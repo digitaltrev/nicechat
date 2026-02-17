4956f75ab29bd95740474a35b7969e9bff9f1c6d
digitaltrev <99693093+digitaltrev@users.noreply.github.com>
2026-02-17 10:31:56 -0700

    Update nice.js

--- nice.js
+++ nice.js
@@ -1,11 +1,10 @@
-
 (function(n,u){
-    window.CXoneDfo=n,
-    window[n]=window[n]||function(){(window[n].q=window[n].q||[]).push(arguments)},window[n].u=u,
-    e=document.createElement("script"),e.type="module",e.src=u+"?"+Math.round(Date.now()/1e3/3600),
-    document.head.appendChild(e)
-    })('cxone','https://web-modules-de-na1.niceincontact.com/loader/1/loader.js');
+  window.CXoneDfo=n,
+  window[n]=window[n]||function(){(window[n].q=window[n].q||[]).push(arguments)},window[n].u=u,
+  e=document.createElement("script"),e.type="module",e.src=u+"?"+Math.round(Date.now()/1e3/3600),
+  document.head.appendChild(e)
+})('cxone','https://web-modules-de-na1.niceincontact.com/loader/1/loader.js');
 
-cxone('init', '5553');
-cxone('guide','init');
-cxone('chat', 'setCaseCustomField', 'authenticated_page', 'no');
+window.cxone('init', '5553');
+window.cxone('guide', 'init');
+window.cxone('chat', 'setCaseCustomField', 'authenticated_page', 'no');
