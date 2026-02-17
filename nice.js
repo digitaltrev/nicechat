7f6138d2d8879d2117499c1c343f7b7a3c36ea99
digitaltrev <99693093+digitaltrev@users.noreply.github.com>
2026-02-17 10:33:20 -0700

    Update nice.js

--- nice.js
+++ nice.js
@@ -1,10 +1,22 @@
+console.log("NICE Script Version: 1.0.2 - Loading...");
+
 (function(n,u){
+  console.log("Initializing wrapper for: " + n);
   window.CXoneDfo=n,
-  window[n]=window[n]||function(){(window[n].q=window[n].q||[]).push(arguments)},window[n].u=u,
-  e=document.createElement("script"),e.type="module",e.src=u+"?"+Math.round(Date.now()/1e3/3600),
-  document.head.appendChild(e)
+  window[n]=window[n]||function(){
+    console.log("Queueing command for " + n + ":", arguments);
+    (window[n].q=window[n].q||[]).push(arguments)
+  },
+  window[n].u=u,
+  e=document.createElement("script"),
+  e.type="module",
+  e.src=u+"?"+Math.round(Date.now()/1e3/3600),
+  document.head.appendChild(e);
+  console.log("Loader script injected: " + e.src);
 })('cxone','https://web-modules-de-na1.niceincontact.com/loader/1/loader.js');
 
+console.log("Executing cxone commands...");
 window.cxone('init', '5553');
 window.cxone('guide', 'init');
 window.cxone('chat', 'setCaseCustomField', 'authenticated_page', 'no');
+console.log("NICE Script Version: 1.0.2 - Execution Complete.");
